003482b2720f88ad95307d8c1c5ece012d79ce42
minagishl <mvxd568.co@icloud.com>
2024-10-21 16:19:09 +0900

    first commit

---
+++ main.ts
@@ -0,0 +1,48 @@
+import { Hono } from "hono";
+import { customAlphabet } from "npm:nanoid";
+
+const app = new Hono();
+const kv = await Deno.openKv();
+
+const nanoid = customAlphabet(
+  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+  7
+);
+
+// Check if the string is a valid URL
+function checkUrl(str: string) {
+  try {
+    new URL(str);
+    return true;
+  } catch (_err) {
+    return false;
+  }
+}
+
+// Shorten a URL
+async function shorten(url: string) {
+  const key = nanoid();
+  await kv.set([key], url);
+  return { key };
+}
+
+app.get("/", (c) => {
+  const url = String(c.req.query("url"));
+  if (url === "undefined") {
+    return c.redirect("https://github.com/evacuate");
+  }
+
+  if (!checkUrl(url)) {
+    return c.json({ error: "Invalid URL" });
+  }
+
+  return shorten(url);
+});
+
+app.get("/:id", async (c) => {
+  const id = c.req.param("id");
+  const storage = await kv.get([id]);
+  return c.redirect(storage.value);
+});
+
+Deno.serve(app.fetch);
